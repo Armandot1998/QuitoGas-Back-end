5180ad18f30bcd928d59cc69e46b5919d7a35694
Armandot1998 <armandotcajilema@gmailcom>
2020-07-17 17:15:14 -0500

    Login y Token
    - Login (generacr token de acceso)
    - Perfil (validacion del acceso del token)

--- src/routes/usuarioRoute.js
+++ src/routes/usuarioRoute.js
@@ -1,12 +1,23 @@
 const { Router } =  require('express');
 const router = Router();
 
+const verifyToken = require('../controllers/verifyToken');
+
 // todas las rutas
 
-const { getUsers} = require('../controllers/index.controller')
-const { createUser} = require('../controllers/index.controller')
+const { getUsers, createUser, 
+    getUserById, updateUser, 
+    infoUser, signinUser} = require('../controllers/usuario.controller');
 
 router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
+router.get('/profile',  verifyToken, infoUser );
+// router.get('/out',  verifyToken, outUser );
 router.post('/users', createUser);
+router.put('/users/:id', updateUser);
+
+router.post('/signin', signinUser);
+
+
 
 module.exports = router;
