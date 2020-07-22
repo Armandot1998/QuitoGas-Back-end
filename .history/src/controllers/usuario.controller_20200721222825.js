cff2f9154d8ceecdae5bc547a81c162e6620a5e2
elias9314 <kuky1993roger@gmail.com>
2020-07-21 22:48:28 -0500

    reconstruccion del update jj

--- .history/src/controllers/usuario.controller_20200721222825.js
+++ .history/src/controllers/usuario.controller_20200721222825.js
@@ -31,10 +31,10 @@ const createUser = async(req, res) => {
 
 const updateUser = async (req, res) => {
     const id = req.params.id;
-    const { id_rol, nombre_usuario, apellido_usuario, cedula_usuario, telefono_usuario, direccion_usuario, correo_usuario, password_usuario  } = req.body.usuario;
-    usuario = await pool.query('update usuario set id_rol = $1, nombre_usuario = $2, apellido_usuario = $3, cedula_usuario = $4,telefono_usuario = $5,direccion_usuario = $6,correo_usuario = $7,password_usuario = $8 where id_usuario = $9', [
-        id_rol, nombre_usuario, apellido_usuario, cedula_usuario, telefono_usuario, direccion_usuario, correo_usuario, password_usuario, id
-    ]);
+    const { nombres, apellidos } = req.body.usuario;
+    usuario = await pool.query('update usuario set nombre_usuario = $1, apellido_usuario = $2 where id_usuario = $3', [
+        nombres, apellidos, id
+    ])
     res.status(200).json({
         menssage: 'Usuario Actualizado',
         auth: true
