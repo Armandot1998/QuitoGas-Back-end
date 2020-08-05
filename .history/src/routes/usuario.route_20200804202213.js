7b22787714d549d442960bfcb1c16b886f35de12
elias9314 <kuky1993roger@gmail.com>
2020-08-05 08:16:49 -0500

    pre-cambio

---
+++ .history/src/routes/usuario.route_20200804202213.js
@@ -0,0 +1,22 @@
+const { Router } =  require('express');
+const router = Router();
+
+const verifyToken = require('../controllers/verifyToken');
+
+// todas las rutas
+
+const { getUsers, createUser, 
+    getUserById, updateUser, 
+    infoUser, signinUser} = require('../controllers/usuario.controller');
+
+router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
+router.get('/profile',  verifyToken, infoUser );
+// router.get('/out',  verifyToken, outUser );
+router.post('/users', createUser);
+router.put('/users', updateUser);
+router.post('/signin', signinUser);
+
+
+
+module.exports = router;
---
+++ .history/src/server_20200724233843.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+//cors
+app.use(cors());
+// midelwares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+// Definiendo rutas
+
+app.use(require('./routes/usuario.route'));
+app.use(require('./routes/sucursalRoute'));
+app.use(require('./routes/productoRoute'))
+
+app.listen(3000);
+console.log('server on port 3000');
