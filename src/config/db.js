8a1a218da6c83b029fdf41503191e164a6323fb6
AlexMejiaArias <alex.mejia.arias@gmail.com>
2020-08-25 13:27:13 -0500

    sucursal add and update

--- src/config/db.js
+++ src/config/db.js
@@ -3,13 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
     hots: 'localhost',
     user: 'postgres',
-<<<<<<< HEAD
     password: 'admin',
     database: 'QuitoGas', 
-=======
-    password: '1234',
-    database: 'gas', 
->>>>>>> 60500c271e95696ec67c40d2bf4981ffe2b60836
     port: '5432'
 })
 
